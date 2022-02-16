2985ca3f6407de0220f77b72564a584d01bd524b
Ari Olvera <ari.olvera@accenture.com>
2022-02-16 12:45:25 -0600

    seccion 3: Programacion Orientada a Objetos

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shark[] sharks = new Shark[]
+            {
+                new Shark("Timbu", 50),
+                new Shark("Jar", 65)
+            };
+
+            IFish[] fishes = new IFish[]
+            {
+                new Siren(100),
+                new Shark("Tib", 50)
+            };
+
+            ShowFish(sharks);
+            ShowAnimals(sharks);
+            ShowFish(fishes);
+        }
+        public static void ShowAnimals(IAnimal[] animals)
+        {
+            Console.WriteLine("-Animals");
+            int i = 0;
+            while (i < animals.Length)
+            {
+                Console.WriteLine(animals[i].Name);
+                i++;
+            }
+        }
+
+        public static void ShowFish(IFish[] fishes)
+        {
+            Console.WriteLine("-Fishes");
+            int i = 0;
+            while (i < fishes.Length)
+            {
+                Console.WriteLine(fishes[i].Swim());
+                i++;
+            }
+        }
+    }
+
+    public class Siren: IFish
+    {
+        public int Speed { get; set; }
+
+        public Siren(int Speed)
+        {
+            this.Speed = Speed;
+        }
+
+        public string Swim()
+        {
+            return $"Siren swims at {Speed} km/h";
+        }
+    }
+
+
+    public class Shark: IAnimal, IFish
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+
+        public Shark(string Name, int Speed)
+        {
+            this.Name = Name;
+            this.Speed = Speed; 
+        }
+        public string Swim()
+        {
+            return $"{Name} swim at {Speed} km/h";
+        }
+    }
+
+    public interface IAnimal
+    {
+        public string Name { get; set; }
+    }
+
+    public interface IFish
+    {
+        public int Speed { get; set; }
+        public string Swim();
+    }
+}
---
+++ Json/Json/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+
+namespace Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beer myBeer = new Beer()
+            {
+                Name = "Pikantus",
+                Brand = "Erdinger"
+            };
+
+            // string json = "{\"Name\": \"Pikantus\", \"Brand\": \"Erdinger\"}";
+
+            string json = JsonSerializer.Serialize(myBeer);
+            Beer beer = JsonSerializer.Deserialize<Beer>(json); 
+
+            Beer[] beers = new Beer[]
+            {
+                new Beer()
+                {
+                    Name = "Pikantus",
+                    Brand = "Erdinger"
+                },
+                new Beer()
+                {
+                    Name = "Corona",
+                    Brand = "Modelo"
+                }
+            };
+
+            /* string jsonTwo = "[" + "{\"Name\": \"Pikantus\", \"Brand\": \"Erdinger\"}," +
+                "{\"Name\": \"Pikantus\", \"Brand\": \"Erdinger\"}" + "]"; */
+
+            string jsonTwo = JsonSerializer.Serialize(beers);
+            Beer[] beersTwo = JsonSerializer.Deserialize<Beer[]>(jsonTwo);
+        }
+
+        public class Beer
+        {
+            public string Name { get; set; }
+            public string Brand { get; set; }   
+        }
+    }
+}
