7bec52976e410771ebceb1043088c67401e516e4
Ari Olvera <ari.olvera@accenture.com>
2022-02-23 09:45:49 -0600

    seccion 6: Excepciones

---
+++ Excepciones/Excepciones/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //string content = File.ReadAllText(@"C:\test.txt");
+                //Console.WriteLine(content);
+
+                throw new Exception("Ocurrio algo raro");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("El archivo no existe");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Aquí me ejecuto pase lo que pase");
+            }
+
+            Console.WriteLine("Aqui se sigue ejecutando");
+
+        }
+    }
+}
